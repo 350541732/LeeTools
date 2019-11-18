bbe87c8307ca6f67768bca9555e6f646fbbd0bcc
LeeSin <350541732@qq.com>
2019-11-18 17:41:32 +0800

    1

---
+++ Leetools/Classes/NewPrint.swift
@@ -0,0 +1,15 @@
+//
+//  NewPrint.swift
+//  aa
+//
+//  Created by 李桂盛 on 2019/11/18.
+//  Copyright © 2019 李桂盛. All rights reserved.
+//
+
+import UIKit
+
+public class NewPrint: NSObject {
+    func printWith(str:String){
+        print(str)
+    }
+}
